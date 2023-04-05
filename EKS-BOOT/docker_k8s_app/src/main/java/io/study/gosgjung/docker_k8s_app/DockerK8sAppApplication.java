161785eb4a91f951d4e874d6d881222633fc281e
gosgjung <gosgjung@gmail.com>
2023-04-06 07:03:48 +0900

    예제 플젝

---
+++ EKS-BOOT/docker_k8s_app/src/main/java/io/study/gosgjung/docker_k8s_app/DockerK8sAppApplication.java
@@ -0,0 +1,13 @@
+package io.study.gosgjung.docker_k8s_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerK8sAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerK8sAppApplication.class, args);
+	}
+
+}
---
+++ EKS-BOOT/docker_k8s_app/src/test/java/io/study/gosgjung/docker_k8s_app/DockerK8sAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.gosgjung.docker_k8s_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerK8sAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
